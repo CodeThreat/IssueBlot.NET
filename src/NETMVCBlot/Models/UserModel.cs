865cba94b715f358b7669e3e6ded3690de1910fb
Bedirhan Urgun <bedirhanurgun@gmail.com>
2021-02-13 21:32:56 +0300

    Adding Exposing Unmasked Sensitive Data test case

---
+++ src/NETMVCBlot/Models/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NETMVCBlot.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "New password")]
+        // CTSECISSUE: ExposingUnmaskedSensitiveData
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}

3a4a9be478a8b7bdf0c227da6803f73490d115b9
can <gurcaglarogulcan@gmail.com>
2021-02-17 14:44:49 +0300

    Test comment examples

--- src/CoreStandaloneBlot/Cryptography/InsecurePBEWorkFactor1.cs
+++ src/CoreStandaloneBlot/Cryptography/InsecurePBEWorkFactor1.cs
@@ -12,18 +12,24 @@ namespace CoreStandaloneBlot.Cryptography
         {
             int workFactor = 8000;
             // CTSECISSUE:InsecureCryptographicHash
+            /* FLOW:Insecure Crypto Hash - Rfc2898DeriveBytes MD5:insecure.cryptographic.hash:0+:FIND_ISSUE:1:
+            */
             using (var pbkdf2 = new Rfc2898DeriveBytes("", new byte[] { }, workFactor, HashAlgorithmName.MD5))
             {
                 var key = pbkdf2.GetBytes(64);
             }
 
             // CTSECISSUE:InsecureCryptographicHash
+            /* FLOW:Insecure Crypto Hash - Rfc2898DeriveBytes SHA1:insecure.cryptographic.hash:0+:FIND_ISSUE:1:
+            */
             using (var pbkdf2 = new Rfc2898DeriveBytes("", new byte[] { }, workFactor, HashAlgorithmName.SHA1))
             {
                 var key = pbkdf2.GetBytes(64);
             }
 
             // CTNONSECISSUE:InsecureCryptographicHash
+            /* FLOW:Insecure Crypto Hash - Rfc2898DeriveBytes SHA256:insecure.cryptographic.hash:0:NONE:1:
+            */
             using (var pbkdf2 = new Rfc2898DeriveBytes("", new byte[] { }, workFactor, HashAlgorithmName.SHA256))
             {
                 var key = pbkdf2.GetBytes(64);
